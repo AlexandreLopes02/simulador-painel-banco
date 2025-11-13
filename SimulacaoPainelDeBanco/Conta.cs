5ade65ae905a74a4dd55ee63ccead59743aafb33
AlexandreLopes02 <alenovoxio@gmail.com>
2025-11-13 08:22:00 -0300

    feat: implementar classe Conta

--- SimulacaoPainelDeBanco/Conta.cs
+++ SimulacaoPainelDeBanco/Conta.cs
@@ -2,4 +2,14 @@
 
 internal class Conta
 {
+    public int Id { get; set; }
+    public string Cpf { get; set; } = string.Empty;     
+    public string Titular { get; set; } = string.Empty;
+    public decimal Saldo { get; set; } = 0m;
+    public DateTime CriadaEm { get; set; } = DateTime.Now;
+
+    public void Exibir()
+    {
+        Console.WriteLine($"Id: {Id} | CPF: {Cpf} | Titular: {Titular} | Saldo: R$ {Saldo:F2}");
+    }
 }
